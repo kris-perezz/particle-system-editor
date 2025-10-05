10c53fceba10a104c902ba5b1977e28825a7f9d0
kris-perezz <kris.ilijay.perez@gmail.com>
2025-10-05 02:43:01 -0600

    feat: implemented circular queue event buffer class

---
+++ src/Events/EventBuffer.cpp
@@ -0,0 +1,52 @@
+#include <src/Events/EventBuffer.h>
+
+namespace SOUP {
+
+  void EventBuffer::enqueue(Event event) {
+    m_buffer[m_head] = event;
+
+    if (m_full) {
+      m_tail = (m_tail + 1) % m_maxCapacity;
+    }
+
+    m_head = (m_head + 1) % m_maxCapacity;
+
+    m_full = m_head == m_tail;
+  }
+
+  std::optional<Event> EventBuffer::dequeue() {
+    if (isEmpty()) {
+      return {};
+    }
+
+    auto event = m_buffer[m_tail];
+    m_full     = false;
+    m_tail     = (m_tail + 1) % m_maxCapacity;
+
+    return event;
+  }
+
+  void EventBuffer::flush() {
+    m_head = m_tail;
+    m_full = false;
+  }
+
+  bool EventBuffer::isEmpty() const { return (!m_full && (m_head == m_tail)); }
+  bool EventBuffer::isFull() const { return m_full; }
+
+  size_t EventBuffer::capacity() const { return m_maxCapacity; }
+  size_t EventBuffer::size() const {
+    size_t size = m_maxCapacity;
+
+    if (!m_full) {
+      if (m_head >= m_tail) {
+        size = m_head - m_tail;
+      } else {
+        size = m_maxCapacity + m_head - m_tail;
+      }
+    }
+
+    return size;
+  }
+
+} // namespace SOUP
---
+++ src/Events/EventBuffer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <src/SOUP_pch.h>
+
+#include <src/Events/Event.h>
+namespace SOUP {
+  class EventBuffer {
+  public:
+    EventBuffer(std::size_t capacity)
+        : m_buffer(std::unique_ptr<Event[]>(new Event[capacity])), m_maxCapacity(capacity) {}
+    ~EventBuffer() = default;
+
+    void enqueue(Event event);
+    std::optional<Event> dequeue();
+
+    void flush();
+    bool isEmpty() const;
+    bool isFull() const;
+
+    size_t capacity() const;
+    size_t size() const;
+
+  private:
+    std::unique_ptr<Event[]> m_buffer;
+    size_t m_head = 0;
+    size_t m_tail = 0;
+    const size_t m_maxCapacity;
+    bool m_full = false;
+  };
+} // namespace SOUP
