df549644d3a3129db8d535cac3889341a782cb7e
kris-perezz <kris.ilijay.perez@gmail.com>
2025-10-07 02:07:22 -0600

    feat: implemented camera class that has orthographic and perspective mode

---
+++ src/Camera.cpp
@@ -0,0 +1,127 @@
+#include <src/Camera.h>
+
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <algorithm>
+
+namespace SOUP {
+  Camera::Camera(CameraType type) : m_cameraType(type) {
+    updateDirection();
+    updateProjection();
+    updateView();
+  }
+
+  void Camera::setCameraType(CameraType type) {
+    m_cameraType = type;
+    updateProjection();
+    updateView();
+  }
+
+  void Camera::setPerspective(float fov, float aspect, float near, float far) {
+    m_cameraType  = CameraType::Perspective;
+    m_fov         = fov;
+    m_aspectRatio = aspect;
+    m_near        = near;
+    m_far         = far;
+    updateProjection();
+  }
+
+  void Camera::setOrthographic(float left, float right, float bottom, float top) {
+    m_cameraType  = CameraType::Orthographic;
+    m_orthoLeft   = left;
+    m_orthoRight  = right;
+    m_orthoBottom = bottom;
+    m_orthoTop    = top;
+    updateProjection();
+  }
+
+  void Camera::setAspectRatio(float aspect) {
+    m_aspectRatio = aspect;
+    if (m_cameraType == CameraType::Perspective) {
+      updateProjection();
+    }
+  }
+
+  void Camera::setClip(float near, float far) {
+    m_near = near;
+    m_far  = far;
+    if (m_cameraType == CameraType::Perspective) {
+      updateProjection();
+    }
+  }
+
+  void Camera::setPosition(const glm::vec3 &position) {
+    m_position = position;
+    updateView();
+  }
+
+  void Camera::setRotation(float yaw, float pitch, float roll) {
+    m_yaw   = yaw;
+    m_pitch = pitch;
+    m_roll  = roll;
+    clampPitch(); 
+    updateDirection();
+    updateView();
+  }
+
+  void Camera::setSpeed(float speed) { m_cameraSpeed = speed; }
+
+  void Camera::setSensitivity(float sensitivity) { m_cameraSensitivity = sensitivity; }
+
+  CameraType Camera::type() const { return m_cameraType; }
+
+  const glm::vec3 &Camera::position() const { return m_position; }
+
+  glm::vec3 Camera::rotation() const { return {m_yaw, m_pitch, m_roll}; }
+
+  float Camera::speed() const { return m_cameraSpeed; }
+
+  const glm::mat4 &Camera::projectionMatrix() const { return m_projection; }
+
+  const glm::mat4 &Camera::viewMatrix() const { return m_view; }
+
+  glm::mat4 Camera::viewProjectionMatrix() const { return m_projection * m_view; }
+
+  void Camera::updateProjection() {
+    if (m_cameraType == CameraType::Perspective) {
+      m_projection = glm::perspective(glm::radians(m_fov), m_aspectRatio, m_near, m_far);
+    } else {
+      m_projection = glm::ortho(m_orthoLeft, m_orthoRight, m_orthoBottom, m_orthoTop, -1.0f, 1.0f);
+    }
+  }
+
+  void Camera::updateView() {
+    if (m_cameraType == CameraType::Orthographic) {
+
+      glm::mat4 translate = glm::translate(glm::mat4(1.0f), m_position);
+      glm::mat4 roll      = glm::rotate(glm::mat4(1.0f), glm::radians(m_roll), glm::vec3(0, 0, 1));
+
+      m_view = glm::inverse(translate * roll);
+
+    } else {
+      const glm::vec3 target = m_position + m_front;
+
+      m_view = glm::lookAt(m_position, target, m_up);
+    }
+  }
+
+  void Camera::updateDirection() {
+    const float yaw   = glm::radians(m_yaw);
+    const float pitch = glm::radians(m_pitch);
+
+    glm::vec3 front;
+    front.x = std::cos(yaw) * std::cos(pitch);
+    front.y = std::sin(pitch);
+    front.z = std::sin(yaw) * std::cos(pitch);
+    m_front = glm::normalize(front);
+
+    m_right = glm::normalize(glm::cross(m_front, m_worldUp));
+    m_up    = glm::normalize(glm::cross(m_right, m_front));
+  }
+
+  void Camera::clampPitch() {
+    m_pitch = std::min(m_pitch, 89.0f);
+    m_pitch = std::max(m_pitch, -89.0f);
+  }
+
+} // namespace SOUP
---
+++ src/Camera.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <src/SOUP_pch.h>
+
+namespace SOUP {
+  enum CameraType : uint8_t {
+    Orthographic = 1,
+    Perspective  = 2,
+  };
+
+  class Camera {
+  public:
+    Camera(CameraType type = CameraType::Orthographic);
+    ~Camera() = default;
+
+    void setCameraType(CameraType type);
+
+    void setPerspective(float fov, float aspect, float near, float far);
+    void setOrthographic(float left, float right, float bottom, float top);
+
+    void setAspectRatio(float aspect);
+    void setClip(float near, float far);
+
+    void setPosition(const glm::vec3 &position);
+    void setRotation(float yaw, float pitch, float roll);
+
+    void setSpeed(float speed);
+    void setSensitivity(float sensitivity);
+
+    CameraType type() const;
+    const glm::vec3 &position() const;
+    glm::vec3 rotation() const;
+    float speed() const;
+    float sensitivity() const;
+
+    const glm::mat4 &projectionMatrix() const;
+    const glm::mat4 &viewMatrix() const;
+    glm::mat4 viewProjectionMatrix() const;
+
+  private:
+    void updateProjection();
+    void updateView();
+    void updateDirection();
+    void clampPitch();
+
+    CameraType m_cameraType;
+
+    glm::vec3 m_position = {0.0f, 0.0f, 0.0f};
+    glm::vec3 m_front    = {0.0f, 0.0f, -1.0f};
+    glm::vec3 m_up       = {0.0f, 1.0f, 0.0f};
+    glm::vec3 m_right    = {1.0f, 0.0f, 0.0f};
+    glm::vec3 m_worldUp  = {0.0f, 1.0f, 0.0f};
+
+    // glm::vec3 m_focalPoint = {0.0f, 0.0f, 0.0f}; // maybe when I have orbit or pan
+
+    glm::mat4 m_projection{1.0f};
+    glm::mat4 m_view{1.0f};
+
+    float m_yaw   = -90.0f;
+    float m_pitch = 0.0f;
+    float m_roll  = 0.0f;
+
+    float m_fov         = 45.0f;
+    float m_aspectRatio = 16.0f / 9.0f; // 16:9
+    float m_near        = 0.1f;
+    float m_far         = 1000.0f;
+
+    float m_orthoLeft{-10.0f};
+    float m_orthoRight{+10.0f};
+    float m_orthoBottom{-10.0f};
+    float m_orthoTop{+10.0f};
+
+    float m_cameraSpeed       = 1.5f;
+    float m_cameraSensitivity = 0.1f;
+  };
+} // namespace SOUP
